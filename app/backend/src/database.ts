b975a950e7002939eff0754bc9e3d5b53aab7c1f
aaa006bd <aalamin@h2701518.stratoserver.net>
2020-09-02 14:34:07 +0100

    removal of typeorm and comments

--- app/backend/src/database.ts
+++ app/backend/src/database.ts
@@ -2,26 +2,8 @@ import { connect } from 'mongoose';
 
 import { Application } from './declarations';
 import Debug from './lib/debug';
-// import logger from './logger';
 
 const debug = Debug('database');
-
-// const RECONNECT_TIMEOUT = 3 * 1000;
-
-// const openDatabase = async (connectionOptions: ConnectionOptions): Promise<Connection> => {
-//   debug(`Create DB connection`, connectionOptions);
-
-//   try {
-//     return await createConnection(connectionOptions);
-//   } catch (error) {
-//     logger.error('Error in opening DB connection!', error);
-//     debug(`Trying to connect again in 3s`);
-
-//     await new Promise((resolve) => setTimeout(resolve, RECONNECT_TIMEOUT));
-
-//     return openDatabase(connectionOptions);
-//   }
-// };
 
 export default async (app: Application): Promise<void> => {
   const dbconfig = app.get('database');
@@ -36,8 +18,6 @@ export default async (app: Application): Promise<void> => {
     auth: { user: dbconfig.username, password: dbconfig.password },
     authSource: 'admin',
   });
-
-  // mongoose.Promise = global.Promise;
 
   debug('Successfully connected to database');
   app.set('db-connected', true);
