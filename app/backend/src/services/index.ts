9da9818622d9aef5f3ff3b475a679b85b5209c5a
aaa006bd <aalamin@h2701518.stratoserver.net>
2020-09-03 15:13:06 +0100

    backend updated and svg-parser added

--- app/backend/src/services/index.ts
+++ app/backend/src/services/index.ts
@@ -1,9 +1,15 @@
 import { Application } from '@/declarations';
 
+import DeskService from './desk.service';
 import HelloService from './hello/hello.service';
+import ReservationService from './reservation.service';
+import RoomService from './room.service';
 import UserService from './user/user.service';
 
 export default function (app: Application): void {
   app.configure(HelloService);
   app.configure(UserService);
+  app.configure(RoomService);
+  app.configure(DeskService);
+  app.configure(ReservationService);
 }
