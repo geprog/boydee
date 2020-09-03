d8d253b74c42d915e7350a6256710f8ec71f505b
aaa006bd <aalamin@h2701518.stratoserver.net>
2020-09-03 10:46:30 +0100

    user relation added in reservation

--- lib/commons/src/entities/Reservation.ts
+++ lib/commons/src/entities/Reservation.ts
@@ -1,10 +1,14 @@
 import { mongoose, prop, Ref } from '@typegoose/typegoose';
 
 import { Desk } from './Desk';
+import { User } from './User';
 
 export class Reservation {
   @prop({ ref: Desk, type: mongoose.Schema.Types.ObjectId })
   desk: Ref<Desk>;
+
+  @prop({ ref: User, type: mongoose.Schema.Types.ObjectId })
+  client: Ref<User>;
 
   @prop()
   start: Date;
