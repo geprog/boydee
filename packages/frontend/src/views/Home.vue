edd832626f526a86a8c63b9c63351055f4e1578c
Anton Bracke <anton@ju60.de>
2020-09-08 00:53:11 +0200

    show rooms if available

--- packages/frontend/src/views/Home.vue
+++ packages/frontend/src/views/Home.vue
@@ -2,7 +2,9 @@
   <div class="home">
     <b-loading is-full-page v-model="loading" can-cancel />
     <template v-if="!loading">
-      <div v-if="hasRooms">Hey</div>
+      <div v-if="hasRooms" class="rooms">
+        <div v-for="room in rooms" :key="room.__id" v-html="room.svg" class="room-svg" />
+      </div>
       <div v-else class="no-room-available">
         <p class="no-rooms-text">You currently have no rooms!</p>
         <v-btn color="primary" @click="$router.push({ name: 'new-room' })">Create a new room</v-btn>
@@ -32,12 +34,19 @@ export default defineComponent({
     return {
       loading,
       hasRooms,
+      rooms,
     };
   },
 });
 </script>
 
 <style scoped>
+.home {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
 .no-room-available {
   display: flex;
   flex-flow: column;
