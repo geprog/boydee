f08a4e1ae3f1046c9b7c43d0414236a5cb341b94
Anton Bracke <anton@ju60.de>
2020-09-07 20:36:55 +0200

    improve feathers-vuex auth

--- packages/frontend/src/store/services/auth.ts
+++ packages/frontend/src/store/services/auth.ts
@@ -2,8 +2,4 @@ import { makeAuthPlugin } from '@/lib/feathers';
 
 export default makeAuthPlugin({
   userService: 'user',
-
-  state: {
-    accessToken: localStorage.getItem('feathers-jwt') || null,
-  },
 });
--- packages/frontend/src/store/services/user.ts
+++ packages/frontend/src/store/services/user.ts
@@ -17,42 +17,12 @@ class User extends BaseModel {
     };
   }
 }
+
 const servicePath = 'user';
 const servicePlugin = makeServicePlugin({
   Model: User,
   service: feathersClient.service(servicePath) as Service<unknown>,
   servicePath,
-});
-
-// Setup the client-side Feathers hooks.
-feathersClient.service(servicePath).hooks({
-  before: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: [],
-  },
-  after: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: [],
-  },
-  error: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: [],
-  },
 });
 
 export default servicePlugin;
